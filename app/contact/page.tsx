fe956586ff4252c6365e700af5e8d92b5d0cd6c0
Naufal Abdussyakur <76672769+Amoebajelek@users.noreply.github.com>
2024-03-03 05:40:50 +0700

    FIx &apos;

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,4 @@
-import { Linkedin, Share2 } from 'lucide-react'
+import { Github, Instagram, Linkedin, Share2 } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -9,12 +9,12 @@ export default function page() {
         <p>Hello 👋🏻, Lorem ipsum dolor sit amet consectetur.</p>
         <h1 className="text-center font-bold text-5xl">Get In Touch!</h1>
         <p className='text-center'>Lorem ipsum dolor sit amet consectetur. Sagittis facilisi urna ultrices risus.Ultrices nisl enim amet ornare eu a felis accumsan. Nisi natoque est lorem eget facilisis.</p>
-        <p>Let's connect with me on below 👇🏻</p>
+        <p>Let&apos;s connect with me on below 👇🏻</p>
       </div>
       <div className="flex gap-8">
         <Link href='https://instagram.com/amoebajelek_' target='_blank'>
           <div className="px-3 py-3 bg-white dark:bg-maDarkGray hover:bg-maBlack dark:hover:bg-maOrange text-maDarkGray dark:text-white hover:text-maOrange rounded-md border border-solid border-maGray dark:border-none shadow-sm">
-            <Share2 className='md:w-8 h-6 md:h-8' />
+            <Instagram className='md:w-8 h-6 md:h-8' />
           </div>
         </Link>
         <Link href='https://linkedin.com/in/naufalabdussyakur' target='_blank'>
@@ -24,7 +24,7 @@ export default function page() {
         </Link>
         <Link href='https://github.com/amoebajelek' target='_blank'>
           <div className="px-3 py-3 bg-white dark:bg-maDarkGray hover:bg-maBlack dark:hover:bg-maOrange text-maDarkGray dark:text-white hover:text-maOrange rounded-md border border-solid border-maGray dark:border-none shadow-sm">
-            <Share2 className='md:w-8 h-6 md:h-8' />
+            <Github className='md:w-8 h-6 md:h-8' />
           </div>
         </Link>
       </div>
