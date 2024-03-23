2a9f60f391f0380e5363ddb205cd973f8f5ffd77
Naufal Abdussyakur <76672769+Amoebajelek@users.noreply.github.com>
2024-03-24 02:09:36 +0700

    Add padding of contents

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="md:ms-[120px] md:me-[20px] w-full min-h-screen flex flex-col gap-8 items-center justify-center bg-white dark:bg-[#252525]">
-      <div className="flex flex-col gap-3 items-center justify-center">
-        <p>Hello 👋🏻, I&apos;m Naufal Abdussyakur and you can call me Alen. I&apos;m a</p>
+      <div className="flex flex-col px-5 py-3 gap-3 items-center justify-center">
+        <p className="text-center">Hello 👋🏻, I&apos;m Naufal Abdussyakur and you can call me Alen. I&apos;m a</p>
         <h1 className="text-center font-bold text-5xl">Fullstack Developer<br />Based in Indonesia</h1>
         <p className="text-center px-5">Enjoy the journey in realizing your desired dreams</p>
       </div>
