66a57774d13e12c594392591ff5824390195e74f
Naufal Abdussyakur <76672769+Amoebajelek@users.noreply.github.com>
2024-03-24 02:53:21 +0700

    Adjust card

--- app/portofolios/page.tsx
+++ app/portofolios/page.tsx
@@ -12,20 +12,20 @@ export const metadata: Metadata = {
 
 export default function page() {
   return (
-    <main className="md:ms-[120px] w-full min-h-screen flex flex-col gap-20 bg-white dark:bg-maBlack pt-40 md:pt-32">
+    <main className="md:ms-[120px] w-full min-h-screen flex flex-col gap-20 bg-white dark:bg-maBlack py-40 md:pt-32">
       <div className="flex flex-col gap-3 items-center justify-center">
         <h1 className="text-center font-bold text-5xl text-maBlack dark:text-white">Find a project that interests you!</h1>
         <p className='text-maDarkGray dark:text-maGray text-center px-5'>I have completed 193 projects which you can see with the project details</p>
       </div>
       <div className="flex gap-5 px-8">
         <Link href='/portofolios/Bragada.ui'>
-          <div className="card card-compact w-full md:w-96 bg-base-100 shadow-xl hover:scale-105 transition-all duration-300 hover:shadow-md">
+          <div className="card card-compact w-full md:w-96 bg-base-100 shadow-xl hover:scale-105 transition-all duration-300 hover:shadow-md rounded-xl overflow-hidden">
             <figure>
               <Image src={Thumbnail} alt='Thumbnail' />
             </figure>
             <div className="card-body p-3">
               <h2 className="card-title text-3xl font-bold text-maOrange">Bragada.ui</h2>
-              <p className='text-maDarkGray'>Designed by <span className='italic font-medium text-maBlack'>Helloalen.dev</span></p>
+              <p className='text-maDarkGray dark:text-maGray'>Designed by <span className='italic font-medium text-maBlack dark:text-white'>Helloalen.dev</span></p>
             </div>
           </div>
         </Link>
