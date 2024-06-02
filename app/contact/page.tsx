6e11df0f22637a8f7b772f105bbe59d5e935cdc7
Amoebajelek <Amoebajelek@gmail.com>
2024-06-02 16:56:23 +0700

    Formating code

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,33 +1,38 @@
-import { Github, Instagram, Linkedin, Share2 } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import { Github, Instagram, Linkedin, Share2 } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import Profile from "@/public/Profile-Alen.png";
 
 export default function page() {
   return (
-    <main className="w-full min-h-screen flex flex-col gap-8 items-center justify-center bg-white dark:bg-[#252525]">
+    <main className="w-full min-h-screen flex flex-col gap-6 items-center justify-center bg-white dark:bg-[#252525]">
+      <div>
+        <Image src={Profile} alt="Profile" />
+      </div>
       <div className="flex flex-col gap-3 items-center justify-center max-w-xl text-center px-5">
         <p>Hello 👋🏻, Lorem ipsum dolor sit amet consectetur.</p>
         <h1 className="font-bold text-5xl">Get In Touch!</h1>
-        <p>Lorem ipsum dolor sit amet consectetur. Sagittis facilisi urna ultrices risus.Ultrices nisl enim amet ornare eu a felis accumsan. Nisi natoque est lorem eget facilisis.</p>
-        <p>Let&apos;s connect with me on below 👇🏻</p>
+        {/* <p>Lorem ipsum dolor sit amet consectetur. Sagittis facilisi urna ultrices risus.Ultrices nisl enim amet ornare eu a felis accumsan. Nisi natoque est lorem eget facilisis.</p> */}
       </div>
-      <div className="flex gap-8">
-        <Link href='https://instagram.com/amoebajelek_' target='_blank'>
-          <div className="px-3 py-3 bg-white dark:bg-maDarkGray hover:bg-maBlack dark:hover:bg-maOrange text-maDarkGray dark:text-white hover:text-maOrange rounded-md border border-solid border-maGray dark:border-none shadow-sm">
-            <Instagram className='md:w-8 h-6 md:h-8' />
+      <p className="mt-5">Let&apos;s connect with me on below 👇🏻</p>
+      <div className="flex gap-4">
+        <Link href="https://instagram.com/amoebajelek_" target="_blank">
+          <div className="px-3 py-3 bg-white dark:bg-maDarkGray hover:bg-maBlack dark:hover:bg-maOrange text-maDarkGray dark:text-white hover:text-maOrange rounded-xl border border-solid border-maGray dark:border-none shadow-sm">
+            <Instagram className="md:w-6 md:h-6" />
           </div>
         </Link>
-        <Link href='https://linkedin.com/in/naufalabdussyakur' target='_blank'>
-          <div className="px-3 py-3 bg-white dark:bg-maDarkGray hover:bg-maBlack dark:hover:bg-maOrange text-maDarkGray dark:text-white hover:text-maOrange rounded-md border border-solid border-maGray dark:border-none shadow-sm">
-            <Linkedin className='md:w-8 h-6 md:h-8' />
+        <Link href="https://linkedin.com/in/naufalabdussyakur" target="_blank">
+          <div className="px-3 py-3 bg-white dark:bg-maDarkGray hover:bg-maBlack dark:hover:bg-maOrange text-maDarkGray dark:text-white hover:text-maOrange rounded-xl border border-solid border-maGray dark:border-none shadow-sm">
+            <Linkedin className="md:w-6 md:h-6" />
           </div>
         </Link>
-        <Link href='https://github.com/amoebajelek' target='_blank'>
-          <div className="px-3 py-3 bg-white dark:bg-maDarkGray hover:bg-maBlack dark:hover:bg-maOrange text-maDarkGray dark:text-white hover:text-maOrange rounded-md border border-solid border-maGray dark:border-none shadow-sm">
-            <Github className='md:w-8 h-6 md:h-8' />
+        <Link href="https://github.com/amoebajelek" target="_blank">
+          <div className="px-3 py-3 bg-white dark:bg-maDarkGray hover:bg-maBlack dark:hover:bg-maOrange text-maDarkGray dark:text-white hover:text-maOrange rounded-xl border border-solid border-maGray dark:border-none shadow-sm">
+            <Github className="md:w-6 md:h-6" />
           </div>
         </Link>
       </div>
     </main>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,29 @@ export default function Home() {
   return (
     <main className="md:ms-[120px] md:me-[20px] w-full min-h-screen flex flex-col gap-8 items-center justify-center bg-white dark:bg-[#252525]">
       <div className="flex flex-col px-5 py-3 gap-3 items-center justify-center">
-        <p className="text-center">Hello 👋🏻, I&apos;m Naufal Abdussyakur and you can call me Alen. I&apos;m a</p>
-        <h1 className="text-center font-bold text-5xl">UI/UX Designer<br />Based in Indonesia</h1>
-        <p className="text-center px-5">Enjoy the journey in realizing your desired dreams</p>
+        <p className="text-center">
+          Hello 👋🏻, I&apos;m Naufal Abdussyakur and you can call me Alen.
+          I&apos;m a
+        </p>
+        <h1 className="text-center font-bold text-5xl">
+          UI/UX Designer
+          <br />
+          Based in Indonesia
+        </h1>
+        <p className="text-center px-5">
+          Enjoy the journey in realizing your desired dreams
+        </p>
       </div>
       <div className="flex gap-3">
-        <Link href='/portofolios'>
-          <p className='px-5 py-3 rounded-full bg-maOrange'>Check My <span className="font-semibold">Projects</span> 🚀</p>
+        <Link href="/portofolios">
+          <p className="px-5 py-3 rounded-full bg-maOrange">
+            Check My <span className="font-semibold">Projects</span> 🚀
+          </p>
         </Link>
-        <Link href='/contact'>
-          <p className='px-5 py-3 rounded-full bg-maGray text-maBlack'><span className="font-semibold">Contact</span> Me</p>
+        <Link href="/contact">
+          <p className="px-5 py-3 rounded-full bg-maGray text-maBlack">
+            <span className="font-semibold">Contact</span> Me
+          </p>
         </Link>
       </div>
     </main>
