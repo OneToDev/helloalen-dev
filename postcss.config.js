4f09a86fc090f55aa4048f287a4abd43428fe159
Amoebajelek <Amoebajelek@gmail.com>
2024-08-08 14:30:11 +0700

    Fix SVG error

--- postcss.config.js
+++ postcss.config.js
@@ -3,18 +3,4 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-        // for webpack 5 use
-        // { and: [/\.(js|ts)x?$/] }
-      },
-
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
 };
